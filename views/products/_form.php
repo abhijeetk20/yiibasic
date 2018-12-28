1bfbd540941210546403d6d98d440bbb09848d0e
abhijeetk20 <abhijeetkhurdal20@gmail.com>
2018-12-28 16:51:40 +0530

    Products Crud Generator

---
+++ views/products/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Products */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="products-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'prodcode')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'prodname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'price')->textInput() ?>
+
+    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/products/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\ProductsSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="products-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'prodid') ?>
+
+    <?= $form->field($model, 'prodcode') ?>
+
+    <?= $form->field($model, 'prodname') ?>
+
+    <?= $form->field($model, 'price') ?>
+
+    <?= $form->field($model, 'image') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
